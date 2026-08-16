07dc45ba2160723c0d53d35c330b5974e5732bb7
Dmytro Zinkevych <dzinkevych@gmail.com>
2026-08-16 16:54:40 +0200

    move VmMetricSeries creation to the util function

--- src/main/kotlin/io/github/dmytrozinkevych/homerstats/Main.kt
+++ src/main/kotlin/io/github/dmytrozinkevych/homerstats/Main.kt
@@ -1,7 +1,7 @@
 package io.github.dmytrozinkevych.homerstats
 
+import io.github.dmytrozinkevych.homerstats.Utils.toMetrics
 import io.github.dmytrozinkevych.homerstats.model.ClimateTelemetryPayload
-import io.github.dmytrozinkevych.homerstats.model.VmMetricSeries
 import io.ktor.client.*
 import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
@@ -11,7 +11,6 @@ import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.request.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import java.time.Instant
 
 private const val PORT = 8000
 private const val VM_IMPORT_URL = "http://localhost:8428/api/v1/import"
@@ -32,27 +31,9 @@ fun main() {
                 with (payload) {
                     println("Received metrics: Timestamp=$timestamp Temperature=$temperature°C, Humidity=$humidity%")
                 }
-                val epochMillis = try {
-                    Instant.parse(payload.timestamp).toEpochMilli()
-                } catch (_: Exception) {
-                    System.currentTimeMillis()
-                }
-                val tempMetric = VmMetricSeries(
-                    metric = mapOf(
-                        "__name__" to "home_temperature_celsius"
-                    ),
-                    values = listOf(payload.temperature),
-                    timestamps = listOf(epochMillis)
-                )
-                val humidityMetric = VmMetricSeries(
-                    metric = mapOf(
-                        "__name__" to "home_humidity_percents"
-                    ),
-                    values = listOf(payload.humidity.toFloat()),
-                    timestamps = listOf(epochMillis)
-                )
+                val metrics = payload.toMetrics()
                 try {
-                    val response = metricsSender.sendMetrics(listOf(tempMetric, humidityMetric))
+                    val response = metricsSender.sendMetrics(metrics)
                     println("Successfully sent metrics to VictoriaMetrics: $response")
                     if (response.status.isSuccess()) {
                         call.respond(HttpStatusCode.NoContent)
